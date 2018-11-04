e45ba78165a87588167885901fc5f2b49b634cef
v1ll4n <v1ll4n@villanch.top>
2018-11-05 01:15:56 +0800

    [feature] publish_json N publish retry

--- arupy/app.py
+++ arupy/app.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 # coding:utf-8
+import json
 import os
-import traceback
-import time
 import threading
+import time
+import traceback
+
 import pika
-from pika import exceptions as pika_exceptions
-from pika.adapters.blocking_connection import BlockingChannel
-from . import outils
+from pika.adapters.blocking_connection import BlockingChannel, BlockingConnection
+
 from . import config as config_utils
+from . import outils
 
 logger = outils.get_logger('arupy')
 
@@ -234,10 +236,11 @@ class ArupyPublisher(object):
 class ArupySafePublisher(object):
     """"""
 
-    def __init__(self, pika_params, confirm=False):
+    def __init__(self, pika_params, confirm=True):
         """Constructor"""
         self.params = pika_params
         self._confirm = confirm
+        self.conn: BlockingConnection = None
         self.chan: BlockingChannel = None
 
         # set conn N chan for self
@@ -259,30 +262,58 @@ class ArupySafePublisher(object):
                 time.sleep(2)
                 continue
 
-    def publish(self, exchange, routing_key, body, properties=None, mandatory=False, immediate=False):
-        while True:
+    def publish(self, exchange, routing_key, body, properties=None, mandatory=False, immediate=False, retry_times=5):
+        for index in range(retry_times):
             try:
                 return self.chan.basic_publish(exchange, routing_key, body, properties, mandatory, immediate)
-            except pika_exceptions.AMQPChannelError as e:
-                self._reset_by_exception(e)
-            except pika_exceptions.ChannelError as e:
-                self._reset_by_exception(e)
-            except pika_exceptions.RecursionError as e:
-                self._reset_by_exception(e)
             except Exception as e:
-                self._reset_by_exception(e)
+                try:
+                    (code, message) = e.args
+                    logger.warning("Met exception! CODE:{} Reason:{}, retry {} times 1s later..".format(
+                        code, message, index + 1
+                    ))
+                except Exception:
+                    logger.warning("Unknown amqp(rabbitmq) exception is raised: {} Type: {}".format(e, type(e)))
+                    logger.exception(e)
+                finally:
+                    time.sleep(1)
+                    self._reset_by_exception(e)
+        return False
 
-            time.sleep(1)
+    def publish_json(self, exchange, routing_key, obj, properties=None, mandatory=False, immediate=False, retry_times=5, **kwargs):
+        body = json.dumps(obj, **kwargs)
+        return self.publish(
+            exchange, routing_key, body, properties, mandatory, immediate, retry_times
+        )
 
-    def close(self):
-        self.chan.close()
-        self.conn.close()
+    def close(self, quiet=False):
+        try:
+            self.chan.close()
+        except Exception as e:
+            if not quiet:
+                logger.warning("closing channel error in SafePublisher. with {} info: {}".format(
+                    type(e), e
+                ))
+
+        try:
+            self.conn.close()
+        except Exception as e:
+            if not quiet:
+                logger.warning("closing connection error in SafePublsiher. with {} info: {}".format(
+                    type(e), e
+                ))
+
+        self.chan = None
+        self.conn = None
 
     def _reset_by_exception(self, e):
         logger.warn("reset publisher by exception: {}, retry!".format(e))
         try:
-            self.close()
+            self.close(quiet=True)
         except Exception:
             pass
+        finally:
+            self.conn = None
+            self.chan = None
 
         self._initial()
---
+++ arupy/tests/test_publisher.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# coding:utf-8
+from ..app import Arupy
+import unittest
+import time
+
+class PublisherTestCase(unittest.TestCase):
+
+    def test_publish_error_test(self):
+        app = Arupy()
+
+        print("create publsiher")
+        publisher = app.new_safe_publisher(True)
+
+        # non-existed exchange will raise 404.
+        start = time.time()
+        publisher.publish("tasssksys", "asdfasdf", "body", retry_times=3)
+        end = time.time()
+
+        self.assertTrue(end - start >= 3)
+
+        # publish to queue is never failed. (emtpy for exchange, queue name for routing_key.)
+        result = publisher.publish("", "joasdfasdfasdfnosc", "asdfasdfasdfasdf", retry_times=2)
+        self.assertTrue(result)
+
+        self.assertTrue(publisher.publish_json("", "asdfasdf", {"tasdfasdfaadsasdfasdf": "asdfasdf"}))
+
+        time.sleep(1)
+
+        publisher.close()
+
+if __name__ == "__main__":
+    unittest.main()
